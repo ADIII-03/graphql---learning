b58dfe850eda80276df7821ee3dc9449aac3e759
ADIII-03 <aradityaraman0518@gmail.com>
2026-04-08 01:42:51 +0530

    prisma setup

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,52 @@
+import "dotenv/config";
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { prisma } from './utils/db.js';
 
 const typeDefs = `#graphql
+  type User {
+    id: Int!
+    firstName: String!
+    lastName: String!
+    email: String!
+  }
+
   type Query {
     hello: String
+  }
+
+  type Mutation {
+    createUser(
+      firstName: String!
+      lastName: String!
+      email: String!
+      password: String!
+    ): User
   }
 `;
 
 const resolvers = {
   Query: {
     hello: () => 'Hello World!',
+  },
+  Mutation: {
+
+    createUser: async (
+      _: any,
+      { firstName, lastName, email, password }: 
+      { firstName: string; lastName: string; email: string; password: string }
+    ) => {
+      const user = await prisma.user.create({
+        data: {
+          firstName,
+          lastName,
+          email,
+          password,
+          salt: '',
+        },
+      });
+      return user;
+    },
   },
 };
 
